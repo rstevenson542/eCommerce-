7012b14cd17626db86deb24862f883ecfc9cd1d0
Robert Stevenson <rstevenson542@gmail.com>
2012-10-06 04:40:41 -0500

    fix admin design

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ShopApp::Application.config.secret_token = 'a50c01630a4f5952cb4de9ca470e94ae62a7374b040ada172330afa7b3e8b5a2f435f05f1c49abdd456e9430acb7251aeb02383c2b685a5744f106db6b4e5cd4'
---
+++ config/locales/Gemfile
@@ -0,0 +1,17 @@
+source 'https://rubygems.org'
+
+gem 'rails', '3.2.8'
+gem 'sqlite3'
+gem 'spree', :git => 'git://github.com/spree/spree.git'
+gem 'haml', '~>3.0'
+
+group :assets do
+  gem 'sass-rails',   '~> 3.2.3'
+  gem 'coffee-rails', '~> 3.2.1'
+  gem 'uglifier', '>= 1.0.3'
+end
+
+gem 'jquery-rails'
+gem 'spree_usa_epay'
+gem 'spree_skrill'
+gem 'spree_auth_devise', :git => 'git://github.com/spree/spree_auth_devise'
